724bf3ff0af1d0a2e6f4e4a5de0523a5de5745dd
markmisener <markmisener@gmail.com>
2017-05-24 20:48:34 -0700

    Update naked twins method in  solution.py and include udacity-pa hidden file

--- sudoku/solution.py
+++ sudoku/solution.py
@@ -40,28 +40,25 @@ def naked_twins(values):
     Returns:
         the values dictionary with the naked twins eliminated from peers.
     """
+    # identify potential naked twins
+    p_twins = [b for b in values.keys() if len(values[b]) == 2]
+    n_twins = [[b1,b2] for b1 in p_twins for b2 in peers[b1] if set(values[b1])==set(values[b2]) ]
 
-    updated_values = values.copy()
-    naked_twins = []
-    for box in updated_values:
-        if len(updated_values[box]) == 2:
-            for peer in peers[box]:
-                if updated_values[peer] == updated_values[box] and box < peer:
-                    naked_twins.append([box, peer])
+    for i in range(len(n_twins)):
+        b_1 = n_twins[i][0]
+        b_2 = n_twins[i][1]
 
-    for n_t in naked_twins:
-        units = [u for u in unitlist if n_t[0] in u and n_t[1] in u]
-        for unit in units:
-            for box in unit:
-                if box != n_t[0] and box != n_t[1]:
-                    updated_values[box] = updated_values[box].replace(updated_values[n_t[0]][0], '')
-                    assign_value(updated_values, box, updated_values[box])
-                    updated_values[box] = updated_values[box].replace(updated_values[n_t[0]][1], '')
-                    assign_value(updated_values, box, updated_values[box])
+        # find peers
+        peers1 = set(peers[b_1])
+        peers2 = set(peers[b_2])
+        peers_int = peers1 & peers2
 
-    if len([box for box in updated_values.keys() if len(updated_values[box]) == 0]):
-        return False
-    return updated_values
+        # remove values from peers
+        for peer_val in peers_int:
+            if len(values[peer_val])>2:
+                for rm_val in values[b_1]:
+                    values = assign_value(values, peer_val, values[peer_val].replace(rm_val,''))
+    return values
 
 def grid_values(grid, show_dots=False):
     """
@@ -105,28 +102,28 @@ def eliminate(values):
     Goes through all the boxes. If a box has only one available value,
     it will remove this value from all the peers of this box.
     '''
-    updated_values = values.copy()
-    solved_values = [box for box in updated_values.keys() if len(updated_values[box]) == 1]
+
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
     for box in solved_values:
-        digit = updated_values[box]
+        digit = values[box]
         for peer in diag_peers[box]:
-            updated_values[peer] = updated_values[peer].replace(digit,'')
-            assign_value(updated_values, peer, updated_values[peer])
-    return updated_values
+            values[peer] = values[peer].replace(digit,'')
+            assign_value(values, peer, values[peer])
+    return values
 
 def only_choice(values):
     '''
     Goes through all the units u. If a unit has a certain value d that will only
     fit in one box of u, it will assign d to this box.
     '''
-    updated_values = values.copy()
+
     for unit in diag_unitlist:
         for digit in digits:
-            dplaces = [box for box in unit if digit in updated_values[box]]
+            dplaces = [box for box in unit if digit in values[box]]
             if len(dplaces) == 1:
-                updated_values[dplaces[0]] = digit
-                assign_value(updated_values, dplaces[0], updated_values[dplaces[0]])
-    return updated_values
+                values[dplaces[0]] = digit
+                assign_value(values, dplaces[0], values[dplaces[0]])
+    return values
 
 def reduce_puzzle(values):
     '''
@@ -134,19 +131,19 @@ def reduce_puzzle(values):
     If at any point, there is a box with zero available values, it will return False.
     Otherwise, the loop will stop whenever the sudoku stays the same during one iteration.
     '''
-    updated_values = values.copy()
+
     solved_values = [box for box in values.keys() if len(values[box]) == 1]
     stalled = False
     while not stalled:
-        solved_values_before = len([box for box in updated_values.keys() if len(updated_values[box]) == 1])
-        updated_values = eliminate(updated_values)
-        updated_values = only_choice(updated_values)
-        updated_values = naked_twins(updated_values)
-        solved_values_after = len([box for box in updated_values.keys() if len(updated_values[box]) == 1])
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        values = eliminate(values)
+        values = only_choice(values)
+        values = naked_twins(values)
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
         stalled = solved_values_before == solved_values_after
-        if len([box for box in updated_values.keys() if len(updated_values[box]) == 0]):
+        if len([box for box in values.keys() if len(values[box]) == 0]):
             return False
-    return updated_values
+    return values
 
 def search(values):
     '''
@@ -154,14 +151,14 @@ def search(values):
     At any given point, it picks the box with fewer available values
     (if there is more than one, it will pick some box), and propagate over that box.
     '''
-    updated_values = reduce_puzzle(values.copy())
-    if updated_values is False:
+    values = reduce_puzzle(values)
+    if values is False:
         return False
-    if all(len(updated_values[s]) == 1 for s in squares):
-        return updated_values
-    n,s = min((len(updated_values[s]), s) for s in squares if len(updated_values[s]) > 1)
-    for value in updated_values[s]:
-        new_sudoku = updated_values.copy()
+    if all(len(values[s]) == 1 for s in squares):
+        return values
+    n,s = min((len(values[s]), s) for s in squares if len(values[s]) > 1)
+    for value in values[s]:
+        new_sudoku = values.copy()
         new_sudoku[s] = value
         attempt = search(new_sudoku)
         if attempt:
@@ -176,8 +173,9 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
-    grid = grid_values(grid)
-    return search(grid)
+    values = grid_values(grid)
+    values = search(values)
+    return values
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
