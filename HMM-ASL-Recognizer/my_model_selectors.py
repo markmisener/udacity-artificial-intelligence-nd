cb8db015b52b27c73f6fb93c6a87d0e42666e340
markmisener <markmisener@gmail.com>
2017-11-08 07:43:02 -0800

    Upload submission files

--- HMM-ASL-Recognizer/my_model_selectors.py
+++ HMM-ASL-Recognizer/my_model_selectors.py
@@ -49,12 +49,10 @@ class ModelSelector(object):
 
 class SelectorConstant(ModelSelector):
     """ select the model with value self.n_constant
-
     """
 
     def select(self):
         """ select based on n_constant value
-
         :return: GaussianHMM object
         """
         best_num_components = self.n_constant
@@ -62,48 +60,115 @@ class SelectorConstant(ModelSelector):
 
 
 class SelectorBIC(ModelSelector):
-    """ select the model with the lowest Bayesian Information Criterion(BIC) score
-
+    """ select the model with the lowest Baysian Information Criterion(BIC) score
     http://www2.imm.dtu.dk/courses/02433/doc/ch6_slides.pdf
     Bayesian information criteria: BIC = -2 * logL + p * logN
     """
-
     def select(self):
         """ select the best model for self.this_word based on
         BIC score for n between self.min_n_components and self.max_n_components
-
         :return: GaussianHMM object
         """
-        warnings.filterwarnings("ignore", category=DeprecationWarning)
+        warnings.filterwarnings("ignore")
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        # initialize best_score and best_model
+        best_score, best_model  = float("inf"), None
+
+        # determine the lowest possible bic score and model
+        for n_components in range(self.min_n_components, self.max_n_components):
+            try:
+                # create the base model
+                model = self.base_model(n_components)
+                # calculate logL
+                logL = model.score(self.X, self.lengths)
+                # determine number of features
+                n_features = self.X.shape[1]
+                # calculate number of data points
+                n_points = n_components * (n_components - 1) + 2 * n_features * n_components
+                # calculate logN
+                logN = np.log(self.X.shape[0])
+                # calculate bic score
+                # BIC = -2 * logL + p * logN
+                bic = -2 * logL + n_points * logN
+                # set best_score and best_model if this is the lowest score so far
+                if bic < best_score:
+                    best_score, best_model = bic, model
+            except Exception as e:
+                continue
+
+        return best_model if best_model else self.base_model(self.n_constant)
 
 
 class SelectorDIC(ModelSelector):
     ''' select best model based on Discriminative Information Criterion
-
     Biem, Alain. "A model selection criterion for classification: Application to hmm topology optimization."
     Document Analysis and Recognition, 2003. Proceedings. Seventh International Conference on. IEEE, 2003.
     http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.58.6208&rep=rep1&type=pdf
-    https://pdfs.semanticscholar.org/ed3d/7c4a5f607201f3848d4c02dd9ba17c791fc2.pdf
     DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
     '''
 
     def select(self):
-        warnings.filterwarnings("ignore", category=DeprecationWarning)
+        """ select the best model for self.this_word based on
+        DIC score for n between self.min_n_components and self.max_n_components
+        :return: GaussianHMM object
+        """
+        warnings.filterwarnings("ignore")
 
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        # create two lists to hold model scores and logs
+        scores, logs = [], []
+
+        try:
+            for n_component in self.n_components:
+                # create model
+                model = self.base_model(n_component)
+                # append model score to logs list
+                logs.append(model.score(self.X, self.lengths))
+            # sum all logs
+            sum_logs = sum(logs)
+            # get length of components
+            m = len(self.n_components)
+            for each_log in logs:
+                # calculate dic score
+                # DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
+                anti_likelihood = (sum_logs - each_log) / (m - 1)
+                scores.append(each_log - anti_likelihood)
+        except Exception as e:
+            pass
+        # determine best score and return corresponding model
+        best_score = self.n_components[np.argmax(scores)] if scores else self.n_constant
+        return self.base_model(best_score)
 
 
 class SelectorCV(ModelSelector):
     ''' select best model based on average log Likelihood of cross-validation folds
-
     '''
 
     def select(self):
-        warnings.filterwarnings("ignore", category=DeprecationWarning)
+        warnings.filterwarnings("ignore")
 
-        # TODO implement model selection using CV
-        raise NotImplementedError
+        # create list to store mean scores
+        mean_scores = []
+
+        # set split method
+        split_method = KFold()
+
+        try:
+            for n_component in self.n_components:
+                # create model
+                model = self.base_model(n_component)
+                # create list to store calculated model mean scores
+                fold_scores = []
+                for _, test in split_method.split(self.sequences):
+                    # calculate test sequences
+                    test_X, test_length = combine_sequences(test, self.sequences)
+                    # append model score to fold scores list
+                    fold_scores.append(model.score(test_X, test_length))
+
+                # append mean of all fold scores to mean scores list
+                mean_scores.append(np.mean(fold_scores))
+        except Exception as e:
+            pass
+
+        # determine best score and return corresponding model
+        best_score = self.n_components[np.argmax(mean_scores)] if mean_scores else self.n_constant
+        return self.base_model(best_score)
--- HMM-ASL-Recognizer/my_recognizer.py
+++ HMM-ASL-Recognizer/my_recognizer.py
@@ -1,14 +1,12 @@
 import warnings
 from asl_data import SinglesData
 
-
 def recognize(models: dict, test_set: SinglesData):
     """ Recognize test word sequences from word models set
-
-   :param models: dict of trained models
+    :param models: dict of trained models
        {'SOMEWORD': GaussianHMM model object, 'SOMEOTHERWORD': GaussianHMM model object, ...}
-   :param test_set: SinglesData object
-   :return: (list, list)  as probabilities, guesses
+    :param test_set: SinglesData object
+    :return: (list, list)  as probabilities, guesses
        both lists are ordered by the test set word_id
        probabilities is a list of dictionaries where each key a word and value is Log Liklihood
            [{SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
@@ -16,10 +14,21 @@ def recognize(models: dict, test_set: SinglesData):
             ]
        guesses is a list of the best guess words ordered by the test set word_id
            ['WORDGUESS0', 'WORDGUESS1', 'WORDGUESS2',...]
-   """
+    """
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     probabilities = []
     guesses = []
-    # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+
+    for word_id in range(0, len(test_set.get_all_Xlengths())):
+        test_word, length = test_set.get_item_Xlengths(word_id)
+        word_log_likelihoods = {}
+
+        for word, model in models.items():
+            try:
+                word_log_likelihoods[word] = model.score(test_word, length)
+            except:
+                word_log_likelihoods[word] = float("-inf")
+        probabilities.append(word_log_likelihoods) # append probabilities
+        guesses.append(max(word_log_likelihoods, key = word_log_likelihoods.get)) # Guesses maximum score
+
+    return probabilities, guesses
